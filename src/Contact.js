50ab8bd48b5e193e27c695dbf1487e7d7f92c5ce
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2022-02-08 23:54:43 +0100

    example of first destructuring of props in Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,13 @@
 import "./Contact.css";
+/* 
+Style zaimportowane do komponentów, zostaną pobrane przez aplikację DOPIERO w momencie renderowania danego komponentu.
+Kiedy już zostaną pobrane przez aplikację, będą one dostępne globalnie dla każdego komponentu (mimo, że w kodzie zostały zaimportowane tylko w jednym komponencie). Nie jest to często porządany efekt, poneważ moze dochodzić do nadpisywania (np. w przypadku tych samych nazw klas).
+*/
 
 export const Contact = (props) => {
+  // destukturyzacja props
+  const { contact } = props;
+  // style
   const tittleStyle = {
     color: "orange",
     fontSize: "20px",
@@ -9,20 +16,18 @@ export const Contact = (props) => {
     <>
       <div>
         <h5 style={tittleStyle}>Tel</h5>
-        <h6 style={{ color: "red", fontWeight: "normal" }}>
-          {props.contact.phone}
-        </h6>
+        <h6 style={{ color: "red", fontWeight: "normal" }}>{contact.phone}</h6>
       </div>
       <div>
         <h5 className="title">Adres</h5>
         <h6>
-          ul. {props.contact.address.street} {props.contact.address.number}
+          ul. {contact.address.street} {contact.address.number}
         </h6>
-        <h6>{props.contact.address.city}</h6>
+        <h6>{contact.address.city}</h6>
       </div>
       <div>
         <h5 className="title">Email</h5>
-        <h6>{props.contact.email}</h6>
+        <h6>{contact.email}</h6>
       </div>
     </>
   );
