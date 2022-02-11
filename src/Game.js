777087e7dff20d17f6f83ad1d041b70e763f8d23
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2022-02-11 23:08:33 +0100

    styled componets with function if - example in Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,54 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Score = styled.span`
+  color: ${(props) => (props.points > 0 ? "yellowgreen" : "pink")};
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 10%;
+  margin-bottom: 100px;
+`;
+
+const Button = styled.button`
+  height: 40px;
+  width: 120px;
+  font-size: 20px;
+  color: black;
+  border: none;
+  border-radius: 4%;
+  color: white;
+  cursor: pointer;
+`;
+
+const AddButton = styled(Button)`
+  background-color: green;
+`;
+
+const RemoveButton = styled(Button)`
+  background-color: red;
+`;
 
 export const Game = (props) => {
   const [points, setPoints] = useState(0);
 
-  const addPoints = () => setPoints(points + 1);
+  const addPoints = () => setPoints(points + 5);
+  const removePoints = () => setPoints(points - 5);
 
   return (
     <>
       <h2>Witamy w grze {props.gameName}</h2>
-      <h4>Troja liczpa punktów to: {points}</h4>
-      <button onClick={addPoints}>Add 1 point</button>
+      <h4>
+        Troja liczpa punktów to:{" "}
+        {/* <span style={{ color: points > 0 ? "green" : "red" }}>{points}</span> */}
+        <Score points={points}>{points}</Score>
+      </h4>
+      <h6>Add or remove points</h6>
+      <ButtonContainer>
+        <AddButton onClick={addPoints}>Add</AddButton>
+        <RemoveButton onClick={removePoints}>Remove</RemoveButton>
+      </ButtonContainer>
     </>
   );
 };
