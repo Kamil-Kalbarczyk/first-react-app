a7e2edf8057547a09bf748225648dc036ebadb8d
Kalbarczyk Kamil <kkalbarczyk@networks.pl>
2022-02-13 22:56:54 +0100

    async await fetching data example in Users.js

--- src/Users.js
+++ src/Users.js
@@ -2,14 +2,30 @@ import { useState, useEffect } from "react"
 
 export const Users = () => {
     const [users, getUsers] =useState(null);
+
+    /* 
+    WAŻNE: useEffect nie może zwrócić promise
+    Poniższy zapis jest niewłaściwy:
+    useEffect(asycn () => {})
+     */
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(resolve => resolve.json())
         .then(data => {getUsers(data); return data})
-        .then(data => console.log(data))
         .catch((error) => console.log(error))
     },[])
 
+    // Poniżej przykład z async await z pobieraniem danych
+   /*  useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const data = await response.json();
+            getUsers(data);
+        }
+
+        fetchData();
+    },[])
+ */
     if(users) {
         return (
             <>
