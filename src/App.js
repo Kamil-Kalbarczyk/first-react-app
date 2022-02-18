8bcdb2dacb2dcf666d6bc6eef41a96351ee11157
Kalbarczyk Kamil <kkalbarczyk@networks.pl>
2022-02-18 22:04:40 +0100

    create Content component and move all components to Content.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import { Wrapper } from "./common/Wrapper";
-import { AboutMe } from "./AboutMe/AboutMe";
-import { Game } from "./games/Game";
-import { GameClass } from "./games/GameClass";
-import { Users } from "./users/Users";
-import { MyForm } from "./forms/MyForm";
-import { MyBoostedForm } from "./forms/MyBoostedForm";
 import { Navigation } from "./navigation/Navigation";
-import { Hello } from "./common/Hello";
-import { Routes, Route } from "react-router-dom";
+import { Content } from "./content/Content";
 
 function App() {
   return (
     <Wrapper>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Hello />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route
-          path="/games"
-          element={
-            <>
-              <Game gameName="Marioo" />
-              <GameClass gameName="Tetrisso" />
-            </>
-          }
-        />
-        <Route path="/users" element={<Users />} />
-        <Route
-          path="/forms"
-          element={
-            <>
-              <MyForm />
-              <MyBoostedForm />
-            </>
-          }
-        />
-      </Routes>
+      <Content />
     </Wrapper>
   );
 }
---
+++ src/content/Content.js
@@ -0,0 +1,38 @@
+import { AboutMe } from "../AboutMe/AboutMe";
+import { Game } from "../games/Game";
+import { GameClass } from "../games/GameClass";
+import { Users } from "../users/Users";
+import { MyForm } from "../forms/MyForm";
+import { MyBoostedForm } from "../forms/MyBoostedForm";
+import { Hello } from "../common/Hello";
+import { Routes, Route } from "react-router-dom";
+
+export const Content = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route
+          path="/games"
+          element={
+            <>
+              <Game gameName="Marioo" />
+              <GameClass gameName="Tetrisso" />
+            </>
+          }
+        />
+        <Route path="/users" element={<Users />} />
+        <Route
+          path="/forms"
+          element={
+            <>
+              <MyForm />
+              <MyBoostedForm />
+            </>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
