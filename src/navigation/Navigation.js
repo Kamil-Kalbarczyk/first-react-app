ef68087a7a8b7807abbf5f1e7332a8bd1c5e583d
Kalbarczyk Kamil <kkalbarczyk@networks.pl>
2022-02-18 21:43:42 +0100

    component props from Material UI use in Navigation.js

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,12 +1,20 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import {Link, NavLink} from 'react-router-dom'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { Link, NavLink } from "react-router-dom";
+
+/* Zwróć uwagę na przyciski w nawigacji.
+Użyliśmy propsa component od Material UI.
+Props component użyty w poniższy sposób:
+<Button component={Link} to='/'>AAA</Button>
+oznacza:
+-wyglądaj jak Button,
+-działaj jak Link 
+ */
 
 export const Navigation = () => {
-  
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -15,31 +23,45 @@ export const Navigation = () => {
             variant="h6"
             noWrap
             component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
             My First <br /> React App
           </Typography>
-              <Button
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              ><Link to='/'>Home</Link>
-              </Button>
-              <Button
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                <Link to='/about'>About me</Link>
-              </Button>
-              <Button
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              ><Link to='/games'>Games</Link>
-              </Button>
-              <Button
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              ><Link to='/forms'>Forms</Link>
-              </Button>
-              <Button
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              ><Link to='/users'>Users</Link>
-              </Button>
+          <Button
+            sx={{ my: 2, color: "white", display: "block" }}
+            component={Link}
+            to="/"
+          >
+            Home
+          </Button>
+          <Button
+            sx={{ my: 2, color: "white", display: "block" }}
+            component={Link}
+            to="/about"
+          >
+            About me
+          </Button>
+          <Button
+            sx={{ my: 2, color: "white", display: "block" }}
+            component={Link}
+            to="/games"
+          >
+            Games
+          </Button>
+          <Button
+            sx={{ my: 2, color: "white", display: "block" }}
+            component={Link}
+            to="/forms"
+          >
+            Forms
+          </Button>
+          <Button
+            sx={{ my: 2, color: "white", display: "block" }}
+            component={Link}
+            to="/users"
+          >
+            Users
+          </Button>
         </Toolbar>
       </Container>
     </AppBar>
