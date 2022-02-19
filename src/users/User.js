43cb2a519361080cd37bac6c2c7f770e8c54931f
Kalbarczyk Kamil <kkalbarczyk@networks.pl>
2022-02-19 01:01:13 +0100

    direct path add to the UserDetails.js

--- src/users/User.js
+++ src/users/User.js
@@ -1,10 +1,26 @@
-export const User = ({name, surname, companyName, phone}) => {
-const noOfChart = phone.indexOf('-');
-if (noOfChart > 0) {
-    const codePhone = '(' + phone.slice(0, noOfChart) + ')' + phone.slice(noOfChart)
-    return <li>{name} {surname} works in {companyName}. Phone: {codePhone}</li>
-}
-return (
-    <li>{name} {surname} works in {companyName}. Phone: {phone}</li>
-)
-}
+import { Link } from "react-router-dom";
+
+export const User = ({ id, name, surname, companyName, phone }) => {
+  const noOfChart = phone.indexOf("-");
+  if (noOfChart > 0) {
+    const codePhone =
+      "(" + phone.slice(0, noOfChart) + ")" + phone.slice(noOfChart);
+    console.log(id);
+    return (
+      <li>
+        <Link to={`/users/${id}`}>
+          {name} {surname}{" "}
+        </Link>
+        works in {companyName}. Phone: {codePhone}
+      </li>
+    );
+  }
+  return (
+    <li>
+      <Link to={`/users/${id}`}>
+        {name} {surname}{" "}
+      </Link>
+      works in {companyName}. Phone: {phone}
+    </li>
+  );
+};
--- src/users/Users.js
+++ src/users/Users.js
@@ -1,24 +1,27 @@
-import {User} from './User'
-import { useState, useEffect } from "react"
+import { User } from "./User";
+import { useState, useEffect } from "react";
 
 export const Users = () => {
-    const [users, getUsers] =useState(null);
+  const [users, getUsers] = useState(null);
 
-    /* 
+  /* 
     WAŻNE: useEffect nie może zwrócić promise
     Poniższy zapis jest niewłaściwy:
     useEffect(asycn () => {})
      */
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(resolve => resolve.json())
-        .then(data => {getUsers(data); return data})
-        .then(data => console.log(data))
-        .catch((error) => console.log(error))
-    },[])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resolve) => resolve.json())
+      .then((data) => {
+        getUsers(data);
+        return data;
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    // Poniżej przykład z async await z pobieraniem danych
-   /*  useEffect(() => {
+  // Poniżej przykład z async await z pobieraniem danych
+  /*  useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('https://jsonplaceholder.typicode.com/users');
             const data = await response.json();
@@ -28,27 +31,30 @@ export const Users = () => {
         fetchData();
     },[])
  */
-    if(users) {
-        return (
-            <>
-            <h3>Users</h3>
-            <ul>
-                {users.map((user) => (
-                    <User key={user.id}
-                    name={user.name}
-                    surname={user.surname}
-                    companyName={user.company.name}
-                    phone ={user.phone}/>
-                ))}
-            </ul>
-            </>
-        )
-    } else {
-        return (
-            <>
-            <h3>Users</h3>
-            <div>Loading...</div>
-            </>
-        )    
-    }
-}
+  if (users) {
+    return (
+      <>
+        <h3>Users</h3>
+        <ul>
+          {users.map((user) => (
+            <User
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              surname={user.surname}
+              companyName={user.company.name}
+              phone={user.phone}
+            />
+          ))}
+        </ul>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3>Users</h3>
+        <div>Loading...</div>
+      </>
+    );
+  }
+};
